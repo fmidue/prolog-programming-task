d93859523c1f0e4da5ac5a52ad12486a5bbf9a20
Marcellus Siegburg <531939+marcellussiegburg@users.noreply.github.com>
2025-10-14 17:24:57 +0200

    fix compilation with recent GHCs

--- src/Prolog/Programming/Data.hs
+++ src/Prolog/Programming/Data.hs
@@ -1,12 +1,24 @@
+{-# LANGUAGE CPP #-}
+#if !MIN_VERSION_base(4,18,0)
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DerivingStrategies #-}
+#endif
 {-# LANGUAGE DeriveGeneric #-}
 module Prolog.Programming.Data where
 
+#if !MIN_VERSION_base(4,18,0)
 import Data.Typeable                    (Typeable)
+#endif
 import GHC.Generics                     (Generic)
 
 newtype Code = Code String
-  deriving (Eq, Generic, Ord, Read, Show, Typeable)
+  deriving (Eq, Generic, Ord, Read, Show)
+#if !MIN_VERSION_base(4,18,0)
+  deriving Typeable
+#endif
 
 newtype Config = Config String
-  deriving (Eq, Generic, Ord, Read, Show, Typeable)
+  deriving (Eq, Generic, Ord, Read, Show)
+#if !MIN_VERSION_base(4,18,0)
+  deriving Typeable
+#endif
